82b546693fc81440382f1ff589d9d44128a15217
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-06-24 16:13:51 +0530

    Refactor code

--- block-BNaabs/server.js
+++ block-BNaabs/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.json())
-app.use(express.urlencoded());
+app.use(express.urlencoded( { extended: false }));
 app.use(express.static(__dirname + '/public'));
 
 
@@ -14,7 +14,7 @@ app.get('/new', (req, res) => {
     res.sendFile(__dirname + '/new.html');
 });
 app.post('/new', (req, res) => {
-    res.send(req.body);
+    res.json(req.body);
 });
 app.get('/users/:username', (req, res) => {
     let username = req.params.username;
