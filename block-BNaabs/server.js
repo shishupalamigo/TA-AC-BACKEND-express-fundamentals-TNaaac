8406f5d3df6d97d4d77f4a0d7a93f9e0ae368d1e
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-06-24 15:59:53 +0530

    Handle requests

---
+++ block-BNaabs/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json())
+app.use(express.urlencoded());
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+});
+app.get('/new', (req, res) => {
+    res.sendFile(__dirname + '/new.html');
+});
+app.post('/new', (req, res) => {
+    res.send(req.body);
+});
+app.get('/users/:username', (req, res) => {
+    let username = req.params.username;
+    res.send(username);
+});
+
+app.listen(4000, () => {
+    console.log('Server listenig on port 4k!');
+});
