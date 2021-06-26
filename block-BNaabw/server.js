ffde3d35d930a3e7448ff107dd9086d3b4e6de3c
Shishupal Kumar <shishupalkr2595@gmail.com>
2021-06-26 11:27:48 +0530

    Add form\json data capture middleware

--- block-BNaabw/server.js
+++ block-BNaabw/server.js
@@ -4,6 +4,9 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public/'));
 
 app.use(logger('dev'));
